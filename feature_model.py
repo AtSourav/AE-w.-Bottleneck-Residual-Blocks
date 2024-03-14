7de2a1bf066d85aea032881f8921aacf31afc365
Sourav <sourav.tphy@gmail.com>
2024-03-14 17:59:22 +0100

    the feature model module was only producing a disconnected graph in it's previous version, rewrote it completely, it seems to work fine now.

--- feature_model.py
+++ feature_model.py
@@ -20,32 +20,46 @@ from keras.applications.resnet_v2 import ResNet50V2
 class feature_model():
     '''
     Instantiates a ResNet50V2 model with weights pre-trained on ImageNet. 
-    The input is an image as a keras tensor which could either be the output
-    of the (V)AE or an original image from the dataset. 
 
     Arguments:
-    -- map_layers: a list of (lower level) layers (specified by integers) that would be used to extract maps.
     -- inp_shape: the shape of the input image, will depend on the dataset we're working on.
                   It should have 3 channels and the height and the width should be no less than 32.
                   For example, (64,64,3) is a valid input shape.
-    -- inp_tensor: the input images to be fed into the feature model in batches.
+    -- num_feature_maps: the number of feature maps we wish to extract.
 
     Attributes:
-    -- feature_maps: a list of feature maps to be extracted from the layers specified.
+    -- base_model: an instance of ResNet50V2 pre-trained on ImageNet.
+    -- conv_layers: a list of names of all the convolutional layers in the base_model.
+    -- feat_layers: a list of names of convolutional layers where the feature maps are extracted from.
+    -- feature_maps: a list of feature maps to be extracted from the layers specified (symbolic Keras tensors).
     -- f_model: a keras Model object that outputs the feature maps.
+    -- output: the output of f_model
+
+    Methods:
+    -- summarise(show_trainable): provides a model summary of f_model. The show-trainable option is True by default.
+                                  It indicates if the layers are trainable or not.
     '''
 
 
-    def __init__(self, map_layers, inp_tensor):
+    def __init__(self, inp_shape, num_feature_maps):
 
-        inp_shape = K.int_shape(inp_tensor)[1:]     # the inp_tensor will be of the shape (batch, H, W, C)
+        self.base_model = ResNet50V2(include_top=False, weights='imagenet', input_shape=inp_shape)
+        self.base_model.trainable = False
 
-        base_model = ResNet50V2(include_top=False, weights='imagenet', input_shape=inp_shape)
-        base_model.trainable = False
+        self.conv_layers = [layer.name for layer in self.base_model.layers if layer.name[-4:]=='conv']
+        m = int(len(self.conv_layers)/2)                 # takes the floor integer
+        s = int(m/num_feature_maps)
+        self.feat_layers = self.conv_layers[s-1:m:s]      # we are taking num_feature_maps number of feature layers
+                                                     # evenly spaced in the first half of the list of all conv layers
+        
+        self.feature_maps = [self.base_model.get_layer(name).output for name in self.feat_layers]
 
-        self.feature_maps = [base_model.layers[i].output for i in map_layers]
+        self.f_model = keras.Model(inputs=self.base_model.input, outputs=self.feature_maps, name='feature_model')
 
-        self.f_model = keras.Model(inp_tensor, self.feature_maps, name='feature_model')
+        self.output = self.f_model.output
+
+    def summarise(self, show_trainable=True):
+        self.f_model.summary(show_trainable=show_trainable)
 
 
 
